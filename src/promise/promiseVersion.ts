dfe3a925d75b8afdf8b2df07cbaa3cc3f193e4cc
Vutlhari23 <vutlhari23m@gamil.com>
2026-09-15 13:34:39 +0200

    [at: implement Promise.all and Promise.race for API request

--- src/promise/promiseVersion.ts
+++ src/promise/promiseVersion.ts
@@ -1,103 +1,93 @@
-import { rejects } from 'assert';
-import {WEATHER_API} from '../api.js'
-import {NEWS_API } from '../api.js'
-import https from 'https'
-import { PassThrough } from 'stream';
+import { WEATHER_API, NEWS_API } from "../api.js";
+import https from "https";
 
 function promiseFetchWeather(): Promise<any> {
-     console.log("fetching the  weather data....");
+    console.log("Fetching the weather data...");
 
-     return new Promise((resolve,reject)=>{
-         
+    return new Promise((resolve, reject) => {
+        https
+            .get(WEATHER_API, (response) => {
+                let weatherData = "";
 
-           https.get(WEATHER_API, (response) => {
+                response.on("data", (chunk) => {
+                    weatherData += chunk;
+                });
 
-              let weatherData = '';
-
-              response.on('data' , (chunk)=> {
-                weatherData+=  chunk;
-              });
-              response.on('end',() => {
-
-                try{
-                const parsedData= JSON.parse(weatherData);
-                resolve(parsedData);
-                }catch(err){
-                    reject(err);
-                }
-              });
-           }).on('error', reject)
-     })};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function fetchWeatherData( ) {
-
-     https.get(WEATHER_API, (res)=> {
-        let  weatherData = '';
-        res.on('data', 
-            (chunk)=> { weatherData += chunk})
-
-            res.on('end', ()=>{
-                const parsedData =JSON.parse(weatherData);
-                console.log(parsedData)
+                response.on("end", () => {
+                    try {
+                        const parsedData = JSON.parse(weatherData);
+                        resolve(parsedData);
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
             })
-     })
-
-
-
+            .on("error", reject);
+    });
 }
-function fetchNews() {
 
-     https.get(NEWS_API, (res)=> {
-        let  newsData = '';
-        res.on('data', 
-            (chunk)=> { newsData += chunk})
+function promiseFetchNews(): Promise<any> {
+    console.log("Fetching the news data...");
 
-            res.on('end', ()=>{
-                const parsedData =JSON.parse(newsData );
-                console.log(parsedData)
+    return new Promise((resolve, reject) => {
+        https
+            .get(NEWS_API, (response) => {
+                let newsData = "";
+
+                response.on("data", (chunk) => {
+                    newsData += chunk;
+                });
+
+                response.on("end", () => {
+                    try {
+                        const parsedData = JSON.parse(newsData);
+                        resolve(parsedData);
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
             })
-     })
-
-
+            .on("error", reject);
+    });
 }
-fetchWeatherData(-23.0476, 30.7167);
-*/
 
+promiseFetchWeather()
+    .then((weatherData) => {
+        console.log("Weather data received:");
+        console.log(weatherData);
+    })
+    .catch((error) => {
+        console.error("Failed to fetch weather:", error);
+    });
 
+promiseFetchNews()
+    .then((newsData) => {
+        console.log("News data received:");
+        console.log(newsData);
+    })
+    .catch((error) => {
+        console.error("Failed to fetch news:", error);
+    });
+
+Promise.all([promiseFetchWeather(), promiseFetchNews()])
+    .then(([weatherData, newsData]) => {
+        console.log("Both requests completed!");
+
+        console.log("Weather:");
+        console.log(weatherData);
+
+        console.log("News:");
+        console.log(newsData);
+    })
+    .catch((error) => {
+        console.error("Something went wrong:", error);
+    });
+
+Promise.race([promiseFetchNews(), promiseFetchWeather()])
+    .then((result) => {
+        console.log("The fastest request finished: ");
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error("Request failed :", error);
+    });
