3c66294e4479813484cd9104fc2ef84aca38a434
Vutlhari23 <vutlhari23m@gamil.com>
2026-09-16 10:08:04 +0200

    feat : implement fetching weather and news using https module with callbacks

---
+++ src/asyncwait/asyncAwaitVersion.ts
@@ -0,0 +1,29 @@
+/*
+
+import { WEATHER_API, NEWS_API } from "../api.js";
+import https from "https";
+
+
+const fetchWeatherData=async  () => {
+    try {
+
+      const response = await fetch(WEATHER_API);
+
+    }catch(error)
+
+
+    }
+    
+
+
+
+
+}
+
+const fetchNews=async  () => {
+    const response = await fetch(NEWS_API);
+
+
+
+}
+*/
--- src/callback/callbackVersion.ts
+++ src/callback/callbackVersion.ts
@@ -3,38 +3,65 @@ import {NEWS_API } from '../api.js'
 import https from 'https'
 
 
-function fetchWeatherData(latitute: number,longitude: number ) {
+const  fetchLocation =()=>{
+   navigator.geolocation.getCurrentPosition ((position) => {
+
+      const  latitude=position.coords.latitude;
+      const longitude =position.coords.longitude;
+      console.log(latitude,longitude);
+   },
+   (error)  =>{
+    console.log(error.message);
+   })}
+
+
+
+const fetchWeatherData =(latitude: number,longitude: number ,
+    callback :(error: Error | null, data?: any) =>void
+) =>{
 
      https.get(WEATHER_API, (res)=> {
         let  weatherData = '';
         res.on('data', 
             (chunk)=> { weatherData += chunk})
-
+            //The end means "The response has now arrived"
             res.on('end', ()=>{
-                const parsedData =JSON.parse(weatherData);
+                try{
+                    const parsedData =JSON.parse(weatherData);
                 console.log(parsedData)
+                callback(null,parsedData)
+                }catch(error){
+                    callback(error as Error)
+                }
+            //Handles the HTTP error incase it fails to fetch the weather 
+            }).on("error", (error)=> {
+                callback(error)
             })
      })
-
-
-
 }
-function fetchNews() {
+const fetchNews =(callback: ( error: Error | null, data?: any)=> void)  =>{  
 
      https.get(NEWS_API, (res)=> {
         let  newsData = '';
         res.on('data', 
-            (chunk)=> { newsData += chunk})
+            (chunk)=> { newsData += chunk}
+        );
+            
+        res.on('end', ()=>{
+                try{
+                    const parsedData =JSON.parse(newsData );
+                    console.log(parsedData)
+                    callback( null, parsedData)
 
-            res.on('end', ()=>{
-                const parsedData =JSON.parse(newsData );
-                console.log(parsedData)
+
+                }catch(error){
+                    callback(error as Error)
+                }
             })
-     })
-
+     }).on("error",(error)=>{callback(error)})
 
 }
-fetchWeatherData(-23.0476, 30.7167);
+
 
 
 
