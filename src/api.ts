a55d30609968e37dde8b95572b244b41a51ebca7
Vutlhari23 <vutlhari23m@gamil.com>
2026-09-17 02:46:27 +0200

    fix: correct geocoding

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 export const WEATHER_API ="https://api.open-meteo.com/v1/forecast?latitude=-23.9&longitude=29.45&current=temperature_2m,wind_speed_10m";
-export const NEWS_API ="https://dummyjson.com/posts";
+export const NEWS_API ="https://dummyjson.com/posts"; 
+export const GEO_URL =`https://open-meteo.com{cityName}&count=1&language=en&format=json`
 
 
--- src/callback/callbackVersion.ts
+++ src/callback/callbackVersion.ts
@@ -1,16 +1,67 @@
+import { parseEnv } from 'util'
 import {WEATHER_API} from '../api.js'
 import {NEWS_API } from '../api.js'
+import { GEO_URL } from '../api.js'
 import https from 'https'
+
+
+//Import and initialize
 import promptSync from 'prompt-sync'
-
-
 const prompt = promptSync();
-const city = prompt('Please enter the city to search :')
+//Get input from the user
+const cityname = prompt('Please enter the city to search :')
+
+const getCityCoordinates =(cityName : string, callback:(error : Error | null, data?:any) => void) =>{  
+   //Using the Https modules to get the city coordinates.
+   const city = cityName.trim();
+   if(!city){
+    callback(new Error("You did not add the name of the city you want to add."));
+    return;
+   }
+const get_coordinates_url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1`;
+
+https.get(get_coordinates_url, (res)=>{
+
+    let cityData='';
+
+    res.on('data',(chunk) => {
+        cityData += chunk;
+    })
+    //The end indicates that the data has been received and we parse the data.
+    res.on('end', ()=>{
+        try{
+           const  data= JSON.parse(cityData);
+           callback(null, data.results[0]);
+           return;
+
+
+        }catch(error){
+            callback(error as Error)
+        }
+    })
+})
+
+}
+//Call the method
+getCityCoordinates(cityname, (error, result) => {
+    if (error) {
+        console.error(error.message);
+    
+        return;
+    }
+
+    console.log(result.latitude, result.longitude);
+});
 
 
 
 
 
+
+
+
+
+/*
 const fetchWeatherData =(latitude: number,longitude: number ,
     callback :(error: Error | null, data?: any) =>void
 ) =>{
@@ -75,7 +126,7 @@ fetchNews((error, newsData)=> {
     }
 
 }) 
-
+*/
 
 
 
